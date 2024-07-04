0e79ebb3dc03f5b1f28798c06e3024644ae6aee1
Vitor Schultz <vitorserta@gmail.com>
2024-07-04 10:00:08 -0300

    cadastro de alunos

--- src/app/cadastro-de-alunos/cadastro-de-alunos.component.ts
+++ src/app/cadastro-de-alunos/cadastro-de-alunos.component.ts
@@ -1,12 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cadastro-de-alunos',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './cadastro-de-alunos.component.html',
   styleUrl: './cadastro-de-alunos.component.css'
 })
 export class CadastroDeAlunosComponent {
+  aluno = {
+    nome: '',
+    cpf: '',
+    email: '',
+    celular: '',
+    curso: ''
+  };
 
+  cursos: string[] = ['Engenharia', 'Medicina', 'Direito', 'Administração', 'Informática'];
+
+  constructor(private router: Router) { }
+
+  onSubmit() {
+    if (this.aluno.nome && this.aluno.cpf && this.aluno.email && this.aluno.celular && this.aluno.curso) {
+      console.log('Dados do aluno:', this.aluno);
+
+      alert('Usuário salvo com sucesso');
+
+      this.router.navigate(['/alunos']);
+    }
+  }
 }
