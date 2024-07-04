0d40e42f8de7a883dc8d3ce99914875be031eaf0
Vitor Schultz <vitorserta@gmail.com>
2024-07-04 09:44:03 -0300

    tela de login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  email: string = '';
+  password: string = '';
 
+  constructor(private router: Router) { }
+
+  onSubmit() {
+    if (this.email && this.password) {
+      this.router.navigate(['/home']);
+    } else {
+      alert('Por favor, preencha todos os campos.');
+    }
+  }
+
+  esqueceuSenha() {
+    alert('Processo de recuperação de senha enviado para o e-mail cadastrado.');
+  }
 }
